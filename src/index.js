0db0bb16b4a2a921aec915ca61099ab6af02164c
MiaChen <miaqwcc@gmail.com>
2021-09-09 10:51:10 +0800

    加入product

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import FilterableProductTable from "./component/FilterableProductTable";
-// import "./index.css";
+import "./index.css";
 // import App from './App';
 // import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
+const PRODUCTS = [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch",
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5",
+  },
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" },
+];
 ReactDOM.render(
-  <React.StrictMode>{<FilterableProductTable />}</React.StrictMode>,
+  <React.StrictMode>
+    {<FilterableProductTable data={PRODUCTS} />}
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
